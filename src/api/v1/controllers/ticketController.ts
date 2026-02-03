8d67fab660b402337fc8d1d57fb53352a692ba13
sania parvej <sparvej@rrc.ca>
2026-02-03 12:33:06 -0600

    feat: add ticket controller with get endpoints.

---
+++ src/api/v1/controllers/ticketController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { HTTP_STATUS } from "../../../constants/httpStatus";
+import * as ticketService from "../services/ticketService";
+
+export function getAll(req: Request, res: Response) {
+  
+    const tickets = ticketService.getAllTickets();
+  
+    res.status(HTTP_STATUS.OK).json(tickets);
+
+}
+
+export function getById(req: Request, res: Response) {
+  const id = Number(req.params.id);
+  const ticket = ticketService.getTicketById(id);
+
+  if (!ticket) {
+    return res
+      .status(HTTP_STATUS.NOT_FOUND)
+      .json({ message: "Ticket not found" });
+  }
+
+  res.status(HTTP_STATUS.OK).json(ticket);
+}
+
+
+
+
