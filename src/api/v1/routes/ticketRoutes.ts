950ef3b2ddbcddbd86cdf89923f1d22c4fff0299
sania parvej <sparvej@rrc.ca>
2026-02-04 08:22:56 -0600

    feat: add ticket routes for get endpoints.

---
+++ src/api/v1/routes/ticketRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import * as controller from "../controllers/ticketController";
+
+const router = Router();
+
+router.get("/", controller.getAll);
+router.get("/:id", controller.getById);
+
+export default router;
